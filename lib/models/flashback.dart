292972782d133260b75fc1139128b0b6b610126e
Demizo <demizodemazo@gmail.com>
2023-09-04 10:01:06 -0500

    Add better flashbacks

---
+++ lib/models/flashback.dart
@@ -0,0 +1,11 @@
+import 'package:daily_you/models/entry.dart';
+
+class Flashback {
+  final String title;
+  final Entry entry;
+
+  const Flashback({
+    required this.title,
+    required this.entry,
+  });
+}
