e3da6421973408699ad1b4693cfe1690b6b0efe2
Demizo <demizodemazo@gmail.com>
2023-12-20 21:21:45 -0600

    Update to v1.7.0

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -880,7 +880,7 @@ class _SettingsPageState extends State<SettingsPage> {
                   icon: const Icon(
                     Icons.new_releases_rounded,
                   ),
-                  label: const Text("1.6.0"),
+                  label: const Text("1.7.0"),
                   onPressed: () async {
                     await launchUrl(
                         Uri.https("github.com", "/Demizo/Daily_You/releases"),
