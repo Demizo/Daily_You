09c6b325fe60132f757651a5883ce5e89d7016f9
Demizo <demizodemazo@gmail.com>
2024-02-02 22:37:07 -0600

    Use markdown widget and markdown toolbar

--- lib/widgets/entry_text_edit.dart
+++ lib/widgets/entry_text_edit.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:markdown_toolbar/markdown_toolbar.dart';
 
 class EntryTextEditor extends StatefulWidget {
   final String text;
@@ -12,18 +14,75 @@ class EntryTextEditor extends StatefulWidget {
 }
 
 class _EntryTextEditorState extends State<EntryTextEditor> {
+  final TextEditingController _controller = TextEditingController();
+  late final FocusNode _focusNode;
+
+  @override
+  void initState() {
+    _controller.text = widget.text;
+    _controller.addListener(() => setState(() {
+          widget.onChangedText(_controller.text);
+        }));
+    _focusNode = FocusNode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      minLines: 5,
-      maxLines: null,
-      initialValue: widget.text,
-      style: const TextStyle(fontSize: 16),
-      decoration: const InputDecoration(
-        border: InputBorder.none,
-        hintText: 'Type something... (supports markdown)',
-      ),
-      onChanged: widget.onChangedText,
+    final theme = Theme.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 4, top: 6, bottom: 0, right: 8),
+          child: MarkdownToolbar(
+            useIncludedTextField: false,
+            controller: _controller,
+            focusNode: _focusNode,
+            width: 36,
+            height: 36,
+            spacing: 4,
+            hideImage: true,
+            hideCode: true,
+            hideLink: false,
+            hideCheckbox: true,
+            hideNumberedList: true,
+            hideHorizontalRule: true,
+            borderRadius: BorderRadius.circular(30),
+            iconColor: theme.colorScheme.onBackground,
+            backgroundColor: theme.cardColor,
+            dropdownTextColor: theme.colorScheme.onBackground,
+            collapsable: false,
+            alignCollapseButtonEnd: false,
+            italicCharacter: '_',
+          ),
+        ),
+        Card(
+            child: Padding(
+          padding: const EdgeInsets.only(left: 8, top: 0, bottom: 0, right: 8),
+          child: TextField(
+            controller: _controller,
+            focusNode: _focusNode,
+            minLines: 5,
+            maxLines: 10,
+            spellCheckConfiguration: SpellCheckConfiguration(
+                spellCheckService: DefaultSpellCheckService()),
+            textCapitalization: TextCapitalization.sentences,
+            style: const TextStyle(fontSize: 16),
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Type something... (supports markdown)',
+            ),
+          ),
+        ))
+      ],
     );
   }
 }
