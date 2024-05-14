5b41e3f34ee25334bea77f6ca86451b7c536c95c
Demizo <demizodemazo@gmail.com>
2024-05-13 21:44:28 -0500

    Add template dropdown

--- lib/widgets/entry_text_edit.dart
+++ lib/widgets/entry_text_edit.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:daily_you/config_manager.dart';
+import 'package:daily_you/entries_database.dart';
+import 'package:daily_you/models/template.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:markdown_toolbar/markdown_toolbar.dart';
@@ -20,6 +22,8 @@ class _EntryTextEditorState extends State<EntryTextEditor> {
   final TextEditingController _controller = TextEditingController();
   final ScrollController _scrollController = ScrollController();
   late final FocusNode _focusNode;
+  late List<Template> templates;
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -28,7 +32,21 @@ class _EntryTextEditorState extends State<EntryTextEditor> {
           widget.onChangedText(_controller.text);
         }));
     _focusNode = FocusNode();
+    loadTemplates();
+
     super.initState();
+  }
+
+  Future loadTemplates() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    templates = await EntriesDatabase.instance.getAllTemplates();
+
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -44,32 +62,59 @@ class _EntryTextEditorState extends State<EntryTextEditor> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (ConfigManager.instance.getField('useMarkdownToolbar'))
-          Padding(
-            padding:
-                const EdgeInsets.only(left: 4, top: 6, bottom: 0, right: 8),
-            child: MarkdownToolbar(
-              useIncludedTextField: false,
-              controller: _controller,
-              focusNode: _focusNode,
-              width: 36,
-              height: 36,
-              spacing: 4,
-              hideImage: true,
-              hideCode: true,
-              hideLink: false,
-              hideCheckbox: true,
-              hideNumberedList: true,
-              hideHorizontalRule: true,
-              borderRadius: BorderRadius.circular(30),
-              iconColor: theme.colorScheme.onBackground,
-              backgroundColor: theme.cardColor,
-              dropdownTextColor: theme.colorScheme.onBackground,
-              collapsable: false,
-              alignCollapseButtonEnd: false,
-              italicCharacter: '_',
-            ),
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            if (ConfigManager.instance.getField('useMarkdownToolbar'))
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 4, top: 6, bottom: 0, right: 8),
+                child: MarkdownToolbar(
+                  useIncludedTextField: false,
+                  controller: _controller,
+                  focusNode: _focusNode,
+                  width: 36,
+                  height: 36,
+                  spacing: 4,
+                  hideImage: true,
+                  hideCode: true,
+                  hideLink: false,
+                  hideCheckbox: true,
+                  hideNumberedList: true,
+                  hideHorizontalRule: true,
+                  borderRadius: BorderRadius.circular(30),
+                  iconColor: theme.colorScheme.onBackground,
+                  backgroundColor: theme.cardColor,
+                  dropdownTextColor: theme.colorScheme.onBackground,
+                  collapsable: false,
+                  alignCollapseButtonEnd: false,
+                  italicCharacter: '_',
+                ),
+              ),
+            if (!isLoading)
+              DropdownButton<int>(
+                  hint: Icon(
+                    Icons.document_scanner_rounded,
+                    color: theme.colorScheme.onBackground,
+                  ),
+                  underline: Container(),
+                  value: ConfigManager.instance.getField("defaultTemplate"),
+                  items: templates.map((Template template) {
+                    return DropdownMenuItem<int>(
+                        value: template.id, child: Text(template.name));
+                  }).toList(),
+                  onChanged: (templateId) async {
+                    var template =
+                        await EntriesDatabase.instance.getTemplate(templateId!);
+                    if (template != null) {
+                      setState(() {
+                        _controller.text = template.text ?? "";
+                        widget.onChangedText(_controller.text);
+                      });
+                    }
+                  })
+          ],
+        ),
         Card(
             child: Padding(
           padding: const EdgeInsets.only(left: 8, top: 2, bottom: 0, right: 8),
