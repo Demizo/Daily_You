dc7e3835127400c3178a41b4c3d84965ef8a8c45
Demizo <demizodemazo@gmail.com>
2024-12-14 15:04:01 -0600

    feat: allow managing templates within entry

--- lib/widgets/template_manager.dart
+++ lib/widgets/template_manager.dart
@@ -44,51 +44,63 @@ class _TemplateManagementDialogState extends State<TemplateManager> {
     );
   }
 
+  Widget _buildTemplatesList() {
+    if (_templates.isEmpty) {
+      return const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text("No templates created..."),
+          ),
+        ],
+      );
+    } else {
+      return ListView.builder(
+        shrinkWrap: true,
+        itemCount: _templates.length,
+        itemBuilder: (context, index) {
+          final template = _templates[index];
+          return Card(
+            child: ListTile(
+              title: Text(template.name),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () async {
+                      await EntriesDatabase.instance
+                          .deleteTemplate(template.id!);
+                      if (ConfigManager.instance.getField("defaultTemplate") ==
+                          template.id!) {
+                        await ConfigManager.instance
+                            .setField("defaultTemplate", -1);
+                      }
+                      _loadTemplates();
+                      widget.onTemplatesUpdated();
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      _showEditTemplatePopup(context, template);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Manage Templates'),
-      content: SizedBox(
-        width: double.maxFinite,
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: _templates.length,
-          itemBuilder: (context, index) {
-            final template = _templates[index];
-            return Card(
-              child: ListTile(
-                title: Text(template.name),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () async {
-                        await EntriesDatabase.instance
-                            .deleteTemplate(template.id!);
-                        if (ConfigManager.instance
-                                .getField("defaultTemplate") ==
-                            template.id!) {
-                          await ConfigManager.instance
-                              .setField("defaultTemplate", -1);
-                        }
-                        _loadTemplates();
-                        widget.onTemplatesUpdated();
-                      },
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: () {
-                        _showEditTemplatePopup(context, template);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+      content: SizedBox(width: double.maxFinite, child: _buildTemplatesList()),
       actions: [
         TextButton(
           child: const Text('Cancel'),
