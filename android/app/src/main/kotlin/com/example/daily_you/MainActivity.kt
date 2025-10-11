96fa4a9a1d1ddfecdd84dfac545ff513bdc57506
Demizo <demizodemazo@gmail.com>
2025-10-10 20:40:00 -0500

    fix: forward onActivityResult
    Not forwarding onActivityResult may cause pickDirectory to fail crashing
    the app.

--- android/app/src/main/kotlin/com/example/daily_you/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/daily_you/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.demizo.daily_you
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import android.content.Intent;
 
 class MainActivity: FlutterFragmentActivity() {
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+  }
 }
