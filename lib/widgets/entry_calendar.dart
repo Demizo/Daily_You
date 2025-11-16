e38ac37c8868315b049b2ae68275cf9a8da8794a
Demizo <73141750+Demizo@users.noreply.github.com>
2025-11-16 15:43:01 -0600

    fix: dont display today's entry outside of the current month (#268)

--- lib/widgets/entry_calendar.dart
+++ lib/widgets/entry_calendar.dart
@@ -53,161 +53,166 @@ class _EntryCalendarState extends State<EntryCalendar>
               color: theme.colorScheme.onSurfaceVariant),
           rightChevronPadding: EdgeInsets.all(8),
           rightChevronMargin: EdgeInsets.only(right: 8)),
-      calendarBuilders: CalendarBuilders(
-        dowBuilder: (context, date) {
-          return FittedBox(
-            fit: BoxFit.scaleDown,
-            child: Center(
-              child: Text(
-                DateFormat("E", TimeManager.currentLocale(context))
-                    .format(date)
-                    .toString(),
-                style: TextStyle(color: theme.colorScheme.onSurfaceVariant),
-              ),
+      calendarBuilders: CalendarBuilders(dowBuilder: (context, date) {
+        return FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Center(
+            child: Text(
+              DateFormat("E", TimeManager.currentLocale(context))
+                  .format(date)
+                  .toString(),
+              style: TextStyle(color: theme.colorScheme.onSurfaceVariant),
             ),
-          );
-        },
-        headerTitleBuilder: (context, date) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                      onPressed: () async {
-                        DateTime? pickedDate = await showDatePicker(
-                          initialDatePickerMode: DatePickerMode.day,
-                          context: context,
-                          initialDate: statsProvider.selectedDate,
-                          firstDate: DateTime.utc(2000),
-                          lastDate: DateTime.now(),
-                        );
-                        if (pickedDate != null) {
-                          statsProvider.selectedDate = pickedDate;
-                          // Update now to jump to the selected day on the calendar
-                          statsProvider.forceUpdate();
-
-                          Entry? pickedEntry =
-                              statsProvider.getEntryForDate(pickedDate);
-
-                          if (pickedEntry != null) {
-                            // Open entry
-                            await Navigator.of(context).push(MaterialPageRoute(
-                              allowSnapshotting: false,
-                              builder: (context) => EntryDetailPage(
-                                  filtered: false,
-                                  index: statsProvider
-                                      .getIndexOfEntry(pickedEntry.id!)),
-                            ));
-                          } else {
-                            // Create new entry
-                            await Navigator.of(context).push(MaterialPageRoute(
-                              allowSnapshotting: false,
-                              builder: (context) => AddEditEntryPage(
-                                overrideCreateDate:
-                                    TimeManager.currentTimeOnDifferentDate(
-                                            pickedDate)
-                                        .copyWith(isUtc: false),
-                              ),
-                            ));
-                          }
-                        }
-                      },
-                      icon: SvgPicture.asset(
-                        'assets/icons/calendar_event.svg',
-                        colorFilter: ColorFilter.mode(
-                            theme.colorScheme.onSurfaceVariant,
-                            BlendMode.srcIn),
-                        width: 24,
-                        height: 24,
-                      )),
-                  GestureDetector(
-                    child: Text(
-                      DateFormat("MMMM y", TimeManager.currentLocale(context))
-                          .format(date)
-                          .toString(),
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    onTap: () async {
-                      final selectedDate = await showYearMonthPicker(context,
-                          initialDate: statsProvider.selectedDate);
-                      if (selectedDate != null) {
-                        statsProvider.selectedDate = selectedDate;
+          ),
+        );
+      }, headerTitleBuilder: (context, date) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                    onPressed: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                        initialDatePickerMode: DatePickerMode.day,
+                        context: context,
+                        initialDate: statsProvider.selectedDate,
+                        firstDate: DateTime.utc(2000),
+                        lastDate: DateTime.now(),
+                      );
+                      if (pickedDate != null) {
+                        statsProvider.selectedDate = pickedDate;
                         // Update now to jump to the selected day on the calendar
                         statsProvider.forceUpdate();
+
+                        Entry? pickedEntry =
+                            statsProvider.getEntryForDate(pickedDate);
+
+                        if (pickedEntry != null) {
+                          // Open entry
+                          await Navigator.of(context).push(MaterialPageRoute(
+                            allowSnapshotting: false,
+                            builder: (context) => EntryDetailPage(
+                                filtered: false,
+                                index: statsProvider
+                                    .getIndexOfEntry(pickedEntry.id!)),
+                          ));
+                        } else {
+                          // Create new entry
+                          await Navigator.of(context).push(MaterialPageRoute(
+                            allowSnapshotting: false,
+                            builder: (context) => AddEditEntryPage(
+                              overrideCreateDate:
+                                  TimeManager.currentTimeOnDifferentDate(
+                                          pickedDate)
+                                      .copyWith(isUtc: false),
+                            ),
+                          ));
+                        }
                       }
                     },
+                    icon: SvgPicture.asset(
+                      'assets/icons/calendar_event.svg',
+                      colorFilter: ColorFilter.mode(
+                          theme.colorScheme.onSurfaceVariant, BlendMode.srcIn),
+                      width: 24,
+                      height: 24,
+                    )),
+                GestureDetector(
+                  child: Text(
+                    DateFormat("MMMM y", TimeManager.currentLocale(context))
+                        .format(date)
+                        .toString(),
+                    style: const TextStyle(fontSize: 18),
                   ),
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  AnimatedSwitcher(
-                    duration: Duration(milliseconds: 300),
-                    transitionBuilder:
-                        (Widget child, Animation<double> animation) {
-                      final scaleAnimation = TweenSequence([
-                        TweenSequenceItem(
-                          tween: Tween<double>(begin: 0.0, end: 1.1)
-                              .chain(CurveTween(curve: Curves.easeOut)),
-                          weight: 50,
-                        ),
-                        TweenSequenceItem(
-                          tween: Tween<double>(begin: 1.1, end: 1.0)
-                              .chain(CurveTween(curve: Curves.easeIn)),
-                          weight: 50,
-                        ),
-                      ]).animate(animation);
+                  onTap: () async {
+                    final selectedDate = await showYearMonthPicker(context,
+                        initialDate: statsProvider.selectedDate);
+                    if (selectedDate != null) {
+                      statsProvider.selectedDate = selectedDate;
+                      // Update now to jump to the selected day on the calendar
+                      statsProvider.forceUpdate();
+                    }
+                  },
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                AnimatedSwitcher(
+                  duration: Duration(milliseconds: 300),
+                  transitionBuilder:
+                      (Widget child, Animation<double> animation) {
+                    final scaleAnimation = TweenSequence([
+                      TweenSequenceItem(
+                        tween: Tween<double>(begin: 0.0, end: 1.1)
+                            .chain(CurveTween(curve: Curves.easeOut)),
+                        weight: 50,
+                      ),
+                      TweenSequenceItem(
+                        tween: Tween<double>(begin: 1.1, end: 1.0)
+                            .chain(CurveTween(curve: Curves.easeIn)),
+                        weight: 50,
+                      ),
+                    ]).animate(animation);
 
-                      return ScaleTransition(
-                        scale: scaleAnimation,
-                        child: child,
-                      );
-                    },
-                    child: (!TimeManager.isSameMonth(
-                            statsProvider.selectedDate, DateTime.now()))
-                        ? IconButton(
-                            key: ValueKey('todayButton'),
-                            onPressed: () async {
-                              statsProvider.selectedDate = DateTime.now();
-                              // Update now to jump to today on the calendar
-                              statsProvider.forceUpdate();
-                            },
-                            icon: SvgPicture.asset(
-                              'assets/icons/calendar_latest.svg',
-                              colorFilter: ColorFilter.mode(
-                                  theme.colorScheme.onSurfaceVariant,
-                                  BlendMode.srcIn),
-                              width: 24,
-                              height: 24,
-                            ))
-                        : SizedBox.shrink(
-                            key: ValueKey('empty')), // Empty widget
-                  ),
-                  const CalendarViewModeSelector(),
-                ],
-              )
-            ],
-          );
-        },
-        defaultBuilder: (context, date, _) {
-          return EntryDayCell(
-            date: date,
-          );
-        },
-        selectedBuilder: (context, date, _) {
-          return EntryDayCell(
-            date: date,
-          );
-        },
-        todayBuilder: (context, date, _) {
-          return EntryDayCell(
-            date: date,
-          );
-        },
-      ),
+                    return ScaleTransition(
+                      scale: scaleAnimation,
+                      child: child,
+                    );
+                  },
+                  child: (!TimeManager.isSameMonth(
+                          statsProvider.selectedDate, DateTime.now()))
+                      ? IconButton(
+                          key: ValueKey('todayButton'),
+                          onPressed: () async {
+                            statsProvider.selectedDate = DateTime.now();
+                            // Update now to jump to today on the calendar
+                            statsProvider.forceUpdate();
+                          },
+                          icon: SvgPicture.asset(
+                            'assets/icons/calendar_latest.svg',
+                            colorFilter: ColorFilter.mode(
+                                theme.colorScheme.onSurfaceVariant,
+                                BlendMode.srcIn),
+                            width: 24,
+                            height: 24,
+                          ))
+                      : SizedBox.shrink(key: ValueKey('empty')), // Empty widget
+                ),
+                const CalendarViewModeSelector(),
+              ],
+            )
+          ],
+        );
+      }, defaultBuilder: (context, date, currentMonth) {
+        return EntryDayCell(
+          date: date,
+          currentMonth: currentMonth,
+        );
+      }, selectedBuilder: (context, date, currentMonth) {
+        return EntryDayCell(
+          date: date,
+          currentMonth: currentMonth,
+        );
+      }, todayBuilder: (context, date, currentMonth) {
+        return EntryDayCell(
+          date: date,
+          currentMonth: currentMonth,
+        );
+      }, disabledBuilder: (context, date, currentMonth) {
+        return EntryDayCell(
+          date: date,
+          currentMonth: currentMonth,
+        );
+      }, outsideBuilder: (context, date, currentMonth) {
+        return EntryDayCell(
+          date: date,
+          currentMonth: currentMonth,
+        );
+      }),
     );
   }
 }
