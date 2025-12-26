c42a0eae6608a08ec9ee0d4a8e62c7e6c64fccbe
Demizo <demizodemazo@gmail.com>
2025-12-26 11:41:41 -0600

    feat: decrease settle time when swiping

--- lib/layouts/mobile_scaffold.dart
+++ lib/layouts/mobile_scaffold.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:daily_you/config_provider.dart';
 import 'package:daily_you/pages/settings/notification_settings.dart';
@@ -114,11 +115,11 @@ class _MobileScaffoldState extends State<MobileScaffold> {
       ]),
       body: PageView(
           controller: _pageController,
-          physics: const ClampingScrollPhysics(),
+          physics: const FastPageViewScrollPhysics(),
           onPageChanged: (index) {
             EasyDebounce.debounce(
                 "page-change",
-                Duration(milliseconds: 150),
+                Duration(milliseconds: 100),
                 () => setState(() {
                       currentIndex = index;
                     }));
@@ -153,6 +154,31 @@ class _MobileScaffoldState extends State<MobileScaffold> {
           ),
         ],
       ),
+    );
+  }
+}
+
+// NOTE: See https://github.com/flutter/flutter/issues/55103#issuecomment-747059541
+class FastPageViewScrollPhysics extends ScrollPhysics {
+  const FastPageViewScrollPhysics({super.parent});
+
+  @override
+  FastPageViewScrollPhysics applyTo(ScrollPhysics? ancestor) {
+    return FastPageViewScrollPhysics(parent: buildParent(ancestor)!);
+  }
+
+  @override
+  SpringDescription get spring => noBounceSpring(0.10);
+
+  SpringDescription noBounceSpring(double settleTimeSeconds) {
+    const mass = 1.0;
+    final stiffness = mass * pow(4 / settleTimeSeconds, 2);
+    final damping = 2 * sqrt(stiffness * mass);
+
+    return SpringDescription(
+      mass: mass,
+      stiffness: stiffness,
+      damping: damping,
     );
   }
 }
