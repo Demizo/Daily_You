a265926c6aaaaab98ec5fa50dd17cbeef6f8d2c3
Demizo <demizodemazo@gmail.com>
2026-07-25 21:12:41 -0500

    feat: use code gen for icon registry

--- lib/l10n/generated/app_localizations_nl.dart
+++ lib/l10n/generated/app_localizations_nl.dart
@@ -462,28 +462,19 @@ class AppLocalizationsNl extends AppLocalizations {
   String get iconGroupMoodPeople => 'Mood & People';
 
   @override
-  String get iconGroupHabitsGoals => 'Habits & Goals';
-
-  @override
-  String get iconGroupActivities => 'Activities';
-
-  @override
   String get iconGroupHealth => 'Health';
 
   @override
   String get iconGroupWorkFinance => 'Work & Finance';
 
   @override
-  String get iconGroupNatureWeather => 'Nature & Weather';
+  String get iconGroupHabitsGoals => 'Habits & Goals';
+
+  @override
+  String get iconGroupNature => 'Nature';
 
   @override
   String get iconGroupFoodDrink => 'Food & Drink';
-
-  @override
-  String get iconGroupHome => 'Home';
-
-  @override
-  String get iconGroupHobbies => 'Hobbies & Entertainment';
 
   @override
   String get iconGroupTravel => 'Travel';
